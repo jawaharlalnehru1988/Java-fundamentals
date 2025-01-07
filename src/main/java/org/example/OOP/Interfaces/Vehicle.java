90563658f069ada69fbe8fae87cf9be9d46236c0
nehru00123@gmail.com <nehru00123@gmail.com>
2025-01-07 15:47:34 +0530

    interfaces are updated with additional features

--- src/main/java/org/example/OOP/Interfaces/Vehicle.java
+++ src/main/java/org/example/OOP/Interfaces/Vehicle.java
@@ -1,5 +1,7 @@
 package org.example.OOP.Interfaces;
 
+import java.util.Arrays;
+
 public interface Vehicle {
     void start();
 
@@ -22,11 +24,31 @@ class Car implements Vehicle{
     }
 }
 
+class Bus implements Vehicle{
+    private int value = 5;
+
+    @Override
+    public void start(){
+        this.value = value*8;
+        System.out.println("this starts with heavy speed: " + this.value);
+    }
+
+    public void fillFullStatus(){
+        int[] counts = {2, 4, 6, 7, 8};
+         int total =  Arrays.stream(counts).sum();
+        System.out.println("this fuel should be filled: " + total);
+    }
+}
+
 class TestCar{
     public static void main(String[] args) {
         Vehicle car = new Car();
         car.fuelUp();
         Vehicle.service();
         car.start();
+
+        Bus bus = new Bus();
+        bus.start();
+        bus.fillFullStatus();
     }
 }
